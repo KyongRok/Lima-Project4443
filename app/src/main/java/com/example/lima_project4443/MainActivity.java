7bf9523f36205f4da9c09d94a01aabc1ccd2c18c
BrianNgu <brian.ngooyen2001@gmail.com>
2024-03-06 00:05:44 -0500

    working on the app, trying to make it work dynamically, might have to hard code instead

--- app/src/main/java/com/example/lima_project4443/MainActivity.java
+++ app/src/main/java/com/example/lima_project4443/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.lima_project4443;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /*
 Group Members:
 KyongRok Kim, 215813413
@@ -12,9 +18,46 @@ Alexis Estropia, 217146473
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static class Shoe {
+        private String name;
+        private int imageResourceId;
+
+        public Shoe(String name, int imageResourceId) {
+            this.name = name;
+            this.imageResourceId = imageResourceId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getImageResourceId() {
+            return imageResourceId;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Shoe pandaShoes = new Shoe("Nike", R.drawable.pandadunks);
+
+        ConstraintLayout itemShoeLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.shoe, null);
+
+        ImageView imageShoe = itemShoeLayout.findViewById(R.id.image_shoe);
+        imageShoe.setImageResource(pandaShoes.getImageResourceId());
+
+        TextView textShoeName = itemShoeLayout.findViewById(R.id.text_shoe_name);
+        textShoeName.setText(pandaShoes.getName());
+
+        ViewGroup mainLayout = findViewById(R.id.main_layout);
+        mainLayout.addView(itemShoeLayout);
+        Log.d("MainActivity", "Main layout ID: " + mainLayout);
+
     }
+
+
 }
