b456e6f09befca67667a68fa12b7e16f600c3c20
captainLEXXX <lex6@my.yorku.ca>
2024-03-23 01:57:42 -0400

    adding Checkout & Payment info activity

---
+++ app/src/main/java/com/example/lima_project4443/Checkout.java
@@ -0,0 +1,130 @@
+package com.example.lima_project4443;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Checkout extends AppCompatActivity {
+
+    private Button buttonPay;
+    private EditText editTextFirstName, editTextLastName, editTextContactPhone, editTextStreetAdd, editTextOptional, editTextCity, editTextPostalCode;
+    private Spinner spinnerProvince;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkout_activity);
+
+        //UI elements initialize
+        buttonPay = findViewById(R.id.buttonPay);
+        editTextFirstName = findViewById(R.id.editfirstName);
+        editTextLastName = findViewById(R.id.editLastName);
+        editTextContactPhone = findViewById(R.id.editContactPhone);
+        editTextStreetAdd = findViewById(R.id.editStreetAdd);
+        editTextOptional = findViewById(R.id.editOptional);
+        editTextCity = findViewById(R.id.editCity);
+        editTextPostalCode = findViewById(R.id.editPostalCode);
+        spinnerProvince = findViewById(R.id.editProvince);
+
+        //define array of Canada provinces
+        String[] canadaProvinces = {"Alberta", "British Columbia", "Manitoba", "New Brunswick", /* Add more provinces as needed */};
+
+        String[] provinces = getResources().getStringArray(R.array.canadaProvinces); //retrieve array of provinces from resource
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, provinces); //create array adapter to populate the spinner with the list of provinces
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerProvince.setAdapter(adapter);
+
+        //set onClickListener for the Proceed to pay button
+        buttonPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String firstName = editTextFirstName.getText().toString();
+                    String lastName = editTextLastName.getText().toString();
+                    String contactPhone = editTextContactPhone.getText().toString();
+                    String streetAdd = editTextStreetAdd.getText().toString();
+                    String optional = editTextOptional.getText().toString();
+                    String city = editTextCity.getText().toString();
+                    String postalCode = editTextPostalCode.getText().toString();
+                    String province = spinnerProvince.getSelectedItem().toString();
+
+                    //check if textfield are all filled in except to optional
+                    if (firstName.trim().isEmpty() || lastName.trim().isEmpty() || contactPhone.trim().isEmpty() || streetAdd.trim().isEmpty() || city.trim().isEmpty() || postalCode.trim().isEmpty() || province.trim().isEmpty())
+                        throw new IllegalArgumentException("Please fill in all the fields");
+
+                    //for test purposes:
+                    String checkoutInfo = "First Name: " + firstName + "\n" +
+                            "Last Name: " + lastName + "\n" +
+                            "Contact Phone: " + contactPhone + "\n" +
+                            "Street Address: " + streetAdd + "\n" +
+                            "Optional: " + optional + "\n" +
+                            "City: " + city + "\n" +
+                            "Postal Code: " + postalCode + "\n" +
+                            "Province: " + province;
+                    // Log the checkout information
+                    System.out.println(checkoutInfo);
+
+                    Intent intent = new Intent(Checkout.this, PaymentInfo.class);
+                    startActivity(intent);
+
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(Checkout.this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //limit mobile number characters to only 10
+        editTextContactPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not used
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Not used
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Limit CVV to 10 characters
+                if (s.length() > 10) {
+                    s.delete(10, s.length());
+                }
+            }
+        });
+
+        //limit postal code characters to only 6
+        editTextPostalCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not used
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Not used
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Limit CVV to 6 characters
+                if (s.length() > 6) {
+                    s.delete(6, s.length());
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/lima_project4443/PaymentInfo.java
@@ -0,0 +1,126 @@
+package com.example.lima_project4443;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PaymentInfo extends AppCompatActivity {
+
+    private Button pay;
+    private EditText firstName, lastName, cardNum, expDate, cvv;
+
+    private CheckBox sameAddressCB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_activity);
+
+        pay = findViewById(R.id.buttonPay);
+        firstName = findViewById(R.id.editfirstName);
+        lastName = findViewById(R.id.editLastName);
+        cardNum = findViewById(R.id.editCardNum);
+        expDate = findViewById(R.id.editExpDate);
+        cvv = findViewById(R.id.editCVV);
+        sameAddressCB = findViewById(R.id.sameAddressCB);
+        sameAddressCB.setChecked(true);
+
+        //set onClickListener for the Pay button
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    String fName = firstName.getText().toString();
+                    String lName = lastName.getText().toString();
+                    String card = cardNum.getText().toString();
+                    String expirationD = expDate.getText().toString();
+                    String codeCVV = cvv.getText().toString();
+
+                    if (fName.trim().isEmpty() || lName.trim().isEmpty() || card.trim().isEmpty() || expirationD.trim().isEmpty() || codeCVV.trim().isEmpty())
+                        throw new IllegalArgumentException("Please fill in all the fields");
+
+                } catch (IllegalArgumentException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(PaymentInfo.this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        //limit card number to 16 digits only
+        cardNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not used
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Not used
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Limit CVV to 3 characters
+                if (s.length() > 16) {
+                    s.delete(16, s.length());
+                }
+            }
+        });
+
+        //limit expiration date to 4 digits only
+        expDate.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Not used
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Limit CVV to 3 characters
+
+                if (s.length() > 4) {
+                    s.delete(4, s.length());
+                }
+            }
+        });
+
+        //limit CVV characters to only 3
+        cvv.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Not used
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Not used
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Limit CVV to 3 characters
+                if (s.length() > 3) {
+                    s.delete(3, s.length());
+                }
+            }
+        });
+    }
+
+}
