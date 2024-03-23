a278817cc78da5bd0d6bd2260c256c24706b8c82
Sean Kim <seankimfeb@gmail.com>
2024-03-23 14:05:15 -0400

    profile in progress

--- app/src/main/java/com/example/lima_project4443/ProfileActivity.java
+++ app/src/main/java/com/example/lima_project4443/ProfileActivity.java
@@ -1,21 +1,84 @@
 package com.example.lima_project4443;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ProfileActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class ProfileActivity extends AppCompatActivity  {
+
+    TextView fname,lname,dob;
+    EditText fnameedit,lnameedit,dobedit;
+
+    UserInfo user = UserInfo.getInstance();
+    private DatePickerDialog.OnDateSetListener mDateListener;
+    boolean edit;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+        Intent intent = getIntent();
+        fname = findViewById(R.id.fnamecontent);
+        lname = findViewById(R.id.lnamecontent);
+        dob = findViewById(R.id.dobcontent);
+        fname.setText(user.getfname());
+        lname.setText(user.getlname());
+        dob.setText(user.getbdate());
+        //dob.setText(String.valueOf(user.getbdate().YEAR)+"-"+String.valueOf(user.getbdate().MONTH)+"-"+String.valueOf(user.getbdate().DATE));
+
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog dialog = new DatePickerDialog(
+                        ProfileActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateListener,
+                        Calendar.YEAR,
+                        Calendar.MONTH,
+                        Calendar.DATE);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+            });
+        mDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String date = year + "/"+ month + "/"+ dayOfMonth;
+                dob.setText(date);
+            }
+        };
+
+
+
+
+
+
+        //String date = Calendar.MONTH + "/"+Calendar.DATE + "/"+ Calendar.YEAR;
+
+
+
+
+
     }
 
 
 
 
 
+
+
 }
+
--- app/src/main/java/com/example/lima_project4443/UserInfo.java
+++ app/src/main/java/com/example/lima_project4443/UserInfo.java
@@ -1,12 +1,61 @@
 package com.example.lima_project4443;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class UserInfo {
     private static UserInfo userInstance = null;
     private String fname;
     private String lname;
-    private int byear;
-    private int bmonth;
-    private int bday;
+    private String bdate;
+    //private int byear;
+   // private int bmonth;
+    //private int bday;
+    private UserInfo(){
+        fname = "Jane";
+        lname = "Doe";
+        bdate = "1989/2/18";
+
+    }
+
+    public static synchronized UserInfo getInstance(){
+        if(userInstance == null){
+            userInstance = new UserInfo();
+        }
+        return userInstance;
+
+    }
+
+    public void reset(){
+        fname = "Jane";
+        lname = "Doe";
+        //Calendar cal = Calendar.getInstance();
+        bdate = "1989/2/18";
+        //bdate = cal;
+
+    }
+    public String getfname(){
+        return this.fname;
+    }
+    public String getlname(){
+        return this.lname;
+    }
+    public String getbdate(){
+        return this.bdate;
+    }
+    public void setfname(String fname){
+        this.fname = fname;
+    }
+    public void setlname(String lname){
+        this.lname = lname;
+    }
+    public void setbdate(int year,int month,int date){
+        bdate=(year +"/"+month+"/"+date);
+    }
+
+
+
+    //method to reset it
 
 
 
